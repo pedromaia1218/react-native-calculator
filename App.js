b8f1a576de361d52d565b65da493d67036991be6
Pedro Henrique Maia <pedromaia1218@gmail.com>
2021-10-28 17:55:57 -0300

    Layout 1.0

--- App.js
+++ App.js
@@ -1,112 +1,63 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
-import type {Node} from 'react';
+import React, { Component } from 'react'
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
   View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
+} from 'react-native'
+import Button from './src/components/Button'
+import Display from './src/components/Display'
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+  buttons: {
+    flexDirection: 'row',
+    flexWrap: 'wrap'
+  }
+})
 
-export default App;
+export default class App extends Component {
+  state = {
+    displayValue: '0'
+  }
+
+  clearMemory = () => {
+    this.setState({ displayValue: '0' })
+  }
+
+  addDigit = (n) => {
+    this.setState({ displayValue: n })
+  }
+
+  setOperation = (operation) => {
+
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Display value={this.state.displayValue} />
+        <View style={styles.buttons}>
+          <Button label='AC' triple onClick={this.clearMemory}/>
+          <Button label='/' operation onClick={this.setOperation}/>
+          <Button label='7' onClick={this.addDigit}/>
+          <Button label='8' onClick={this.addDigit}/>
+          <Button label='9' onClick={this.addDigit}/>
+          <Button label='*' operation onClick={this.setOperation}/>
+          <Button label='4' onClick={this.addDigit}/>
+          <Button label='5' onClick={this.addDigit}/>
+          <Button label='6' onClick={this.addDigit}/>
+          <Button label='-' operation onClick={this.setOperation}/>
+          <Button label='1' onClick={this.addDigit}/>
+          <Button label='2' onClick={this.addDigit}/>
+          <Button label='3' onClick={this.addDigit}/>
+          <Button label='+' operation onClick={this.setOperation}/>
+          <Button label='0' double onClick={this.addDigit}/>
+          <Button label='.' onClick={this.addDigit}/>
+          <Button label='=' operation onClick={this.setOperation}/>
+        </View>
+      </View>
+    )
+  }
+}
